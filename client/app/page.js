aa6f79c9c708ffb7c67380256d0d4785c4b12270
Azra Ali <azra_ali09@outlook.com>
2025-11-15 18:21:52 -0500

    finished the images/buttons (figured out the gaps and margins)

--- client/app/page.js
+++ client/app/page.js
@@ -59,13 +59,13 @@ export default function Home() {
       {!code && 
         <>
           <div className = "flex w-screen justify-center">
-          <img className = "mt-2 w-3/4 h-3/4" src={title.src}/>
+          <img className = "mt-2 w-1/2 h-1/2" src={title.src}/>
           </div>
-          <div className="flex flex-col">
-          <button className="p-1 bg-indigo-500 rounded-md w-32" onClick={createRoom}>Create room</button>
-          <div className="mt-10">
-            <input placeholder="Room Code" className="bg-white text-blue-600 p-1" onChange={(e) => {setCodeInput(e.target.value)}}></input>
-            <button className= "p-1 bg-indigo-500" onClick={(joinRoom)}>Join Room</button>
+          <div className="flex flex-col justify-center">
+          <button className="p-1 bg-indigo-500 rounded-md w-32 ml-20" onClick={createRoom}>Create room</button>
+          <div className="mt-7 flex gap-4 mb-5">
+            <input placeholder="Room Code" className="bg-white text-blue-600 p-1 rounded-md " onChange={(e) => {setCodeInput(e.target.value)}}></input>
+            <button className= "p-1 bg-indigo-500 rounded-md" onClick={(joinRoom)}>Join Room</button>
           </div>
           </div>
         </>}
