43912538b8e21e0f8e60ef37afffe613a94ea491
Thuc Chi Do <thucchi.code@gmail.com>
2025-11-15 23:53:34 -0500

    update waiting room

--- client/app/[code]/waiting/page.js
+++ client/app/[code]/waiting/page.js
@@ -2,6 +2,10 @@
 import {useEffect, useState} from "react";
 import { useParams } from "next/navigation";
 import getSocket from "../../socket";
+import cat1 from "../../images/cat1.png";
+import cat2 from "../../images/cat2.png";
+import cat3 from "../../images/cat3.png";
+import cat4 from "../../images/cat4.png";
 
 export default function waitingRoom() {
     // Get url params
@@ -14,6 +18,7 @@ export default function waitingRoom() {
     const [players, setPlayers] = useState([]);
     const [editName, setEditName] = useState(false);
     const [name, setName] = useState("");
+    const images = [cat1.src, cat2.src, cat3.src, cat4.src];
     
     useEffect(() => {
         ws.onmessage = (event) => {
@@ -51,30 +56,30 @@ export default function waitingRoom() {
     }
 
     return (
-        <>
-            <p>what</p>
-            <p>sup</p>
-            <p>{code}</p>
-            <p>Number of players: {playersNum}</p>
-            <p>Player ID: {playerID}</p>
-            {players.map((item, index) => (
-                <div key={index}>
-                    {(editName && (item.id === playerID)) &&
-                        <>  
-                            <input className="bg-white text-blue-800" value={name} onChange={(e) => {setName(e.target.value)}}></input>
-                            <button onClick={saveName}>✓</button>
-                        </>
-                    }
-                    {!(editName && (item.id === playerID)) &&
-                        <>
-                            <p>{item.name}</p>
-                            {item.id === playerID && 
-                                <button onClick={openEditName}>:edit:</button>
-                            }
-                        </>
-                    }
-                </div>
-            ))}
-        </>
+        <div className = "flex flex-col max-w-screen min-h-screen justify-center items-center bg-blue-300">
+            <p>Code: {code}</p>
+            <button className = "pl-10 pr-10 p-2 rounded-md bg-indigo-600 mt-20 mb-25 text-5xl">Start!</button>
+            <div className = "flex row-span-2 max-w-screen justify-center px-10">
+                {players.map((item, index) => (
+                    <div key={index} className = "w-max flex flex-col text-center items-center">
+                        <img className = "mt-2" src={images[index]}/>
+                        {(editName && (item.id === playerID)) &&
+                            <>  
+                                <input className="text-center p-1 border rounded w-fit mr-5" value={name} onChange={(e) => {setName(e.target.value)}}></input>
+                                <button onClick={saveName}>✓</button>
+                            </>
+                        }
+                        {!(editName && (item.id === playerID)) &&
+                            <>
+                                <p className="text-center p-1 w-fit mr-5">{item.name}</p>
+                                {item.id === playerID && 
+                                    <button onClick={openEditName}>:edit:</button>
+                                }
+                            </>
+                        }
+                    </div>
+                ))}
+            </div>        
+        </div>
     )
 }
