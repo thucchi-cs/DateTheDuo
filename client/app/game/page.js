34bf7b2f01e63bf11115527c8afd951f2609e970
Azra Ali <azra_ali09@outlook.com>
2025-11-16 01:35:15 -0500

    fixed up the layout/edit

--- client/app/game/page.js
+++ client/app/game/page.js
@@ -1,41 +1,41 @@
-"use client";
-import {useRef, useEffect, useState} from "react";
-import angryDuo from "../images/angryDuo.png";
-import chadDuo from "../images/chadDuo.png";
-import dateDuo from "../images/dateDuo.png";
-import fancyDuo from "../images/fancyDuo.png";
-import inloveDuo from "../images/inloveDuo.png";
-import wowzaDuo from "../images/wowzaDuo.png";
-import stage from "../images/stage.png";
-import question from '../images/questionBank.png';
-import spotlight from '../images/spotlight.png';
+// "use client";
+// import {useRef, useEffect, useState} from "react";
+// import angryDuo from "../images/angryDuo.png";
+// import chadDuo from "../images/chadDuo.png";
+// import dateDuo from "../images/dateDuo.png";
+// import fancyDuo from "../images/fancyDuo.png";
+// import inloveDuo from "../images/inloveDuo.png";
+// import wowzaDuo from "../images/wowzaDuo.png";
+// import stage from "../images/stage.png";
+// import question from '../images/questionBank.png';
+// import spotlight from '../images/spotlight.png';
 
-export default function Home(){
-    return(
-        <div className = "flex flex-col max-w-screen min-h-screen justify-center items-center bg-blue-300">
-        <button className = "pl-10 pr-10 p-2 rounded-md bg-indigo-600 mt-20 mb-25 text-5xl">Start!</button>
-        <div className = "flex row-span-2 max-w-screen justify-center px-10">
-            <div className = "w-max flex flex-col text-center items-center">
-            <img className = "mt-2" src={stage.src}/>
-            <input className="text-center p-1 border rounded w-fit mr-5"></input>
-            </div>
-            <div className = "w-max flex flex-col text-center items-center">
-                <img className = "mt-2" src={stage.src}/>
-            <input className="text-center p-1 border rounded w-fit"></input>
-            </div>
-            <div className = "w-max flex flex-col text-center items-center">
-                <img className = "mt-2"src={stage.src}/>
-                <input className = "text-center p-1 border rounded w-fit"></input>
-            </div>
-            <div className ="w-max flex flex-col text-center items-center">
-                <img className = "mt-2" src={stage.src}/>
-                <input className = "text-center p-1 border rounded w-fit"></input>
-            </div>
+// export default function Home(){
+//     return(
+//         <div className = "flex flex-col max-w-screen min-h-screen justify-center items-center bg-blue-300">
+//         <button className = "pl-10 pr-10 p-2 rounded-md bg-indigo-600 mt-20 mb-25 text-5xl">Start!</button>
+//         <div className = "flex row-span-2 max-w-screen justify-center px-10">
+//             <div className = "w-max flex flex-col text-center items-center">
+//             <img className = "mt-2" src={stage.src}/>
+//             <input className="text-center p-1 border rounded w-fit mr-5"></input>
+//             </div>
+//             <div className = "w-max flex flex-col text-center items-center">
+//                 <img className = "mt-2" src={stage.src}/>
+//             <input className="text-center p-1 border rounded w-fit"></input>
+//             </div>
+//             <div className = "w-max flex flex-col text-center items-center">
+//                 <img className = "mt-2"src={stage.src}/>
+//                 <input className = "text-center p-1 border rounded w-fit"></input>
+//             </div>
+//             <div className ="w-max flex flex-col text-center items-center">
+//                 <img className = "mt-2" src={stage.src}/>
+//                 <input className = "text-center p-1 border rounded w-fit"></input>
+//             </div>
 
 
-        </div>
+//         </div>
         
-        </div>
+//         </div>
 
-    );
-}
+//     );
+// }
